1351183984b4701719d2b9f065b45bb497f2ceb5
Ripan <ripan.kumar@digital.cabinet-office.gov.uk>
2025-04-19 21:49:00 +0100

    Update user-signup-api-cluster.tf

--- govwifi-api/user-signup-api-cluster.tf
+++ govwifi-api/user-signup-api-cluster.tf
@@ -67,7 +67,8 @@ resource "aws_iam_role_policy" "user_signup_api_task_policy" {
         "s3:HeadBucket"
       ],
       "Resource": [
-        "*"
+        "arn:aws:s3:::${var.env_name}-notification-templates",
+        "arn:aws:s3:::${var.env_name}-notification-templates/*"
       ]
     }
   ]
