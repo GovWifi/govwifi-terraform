d977c7cff80473631c27b23b8301af5b6a9c5e44
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2025-07-29 14:35:42 +0100

    feature: [GW-2335] add autoscaler policy

--- govwifi-frontend/autoscaling-policy.tf
+++ govwifi-frontend/autoscaling-policy.tf
@@ -16,13 +16,12 @@ resource "aws_appautoscaling_policy" "ecs_service_radius_frontend_load_scale_up_
   step_scaling_policy_configuration {
     adjustment_type = "ChangeInCapacity" # Add/remove a fixed number of tasks
     cooldown = 300 # Cooldown period in seconds
-    metric_aggregation_type = "Sum" # Sum of metric values over the period
+    metric_aggregation_type = "Average"
 
     step_adjustment {
       metric_interval_lower_bound = 0 # If metric value is > 0
       scaling_adjustment = 1 # Add 1 task
     }
-    # You can add more step adjustments for more aggressive scaling
   }
   depends_on = [aws_appautoscaling_target.ecs_radius_frontend_target]
 }
@@ -37,56 +36,12 @@ resource "aws_appautoscaling_policy" "ecs_service_radius_frontend_load_scale_dow
   step_scaling_policy_configuration {
     adjustment_type = "ChangeInCapacity" # Add/remove a fixed number of tasks
     cooldown = 3600 # Cooldown period in seconds, leave grace period for traffic to calm down before reducing tasks
-    metric_aggregation_type = "Sum" # Sum of metric values over the period
+    metric_aggregation_type = "Average"
 
     step_adjustment {
       metric_interval_lower_bound = 0 # If metric value is > 0
       scaling_adjustment = -1 # Remove 1 task
     }
-    # You can add more step adjustments for more aggressive scaling
   }
   depends_on = [aws_appautoscaling_target.ecs_radius_frontend_target]
 }
-
-/*
-resource "aws_appautoscaling_policy" "ecs_policy_up_radiusentication_api" {
-  name               = "ECS Scale Up"
-  service_namespace  = "ecs"
-  policy_type        = "StepScaling"
-  resource_id        = "service/${aws_ecs_cluster.api_cluster.name}/${aws_ecs_service.load_balanced_frontend_service.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    metric_aggregation_type = "Average"
-
-    step_adjustment {
-      metric_interval_lower_bound = 0
-      scaling_adjustment          = 1
-    }
-  }
-
-  depends_on = [aws_appautoscaling_target.radius_ecs_target_radiusentication_api]
-}
-
-
-resource "aws_appautoscaling_policy" "ecs_policy_down_radiusentication_api" {
-  name               = "ECS Scale Down"
-  service_namespace  = "ecs"
-  resource_id        = "service/${aws_ecs_cluster.api_cluster.name}/${aws_ecs_service.radiusentication_api_service.name}"
-  policy_type        = "StepScaling"
-  scalable_dimension = "ecs:service:DesiredCount"
-
-  step_scaling_policy_configuration {
-    adjustment_type         = "ChangeInCapacity"
-    metric_aggregation_type = "Average"
-
-    step_adjustment {
-      metric_interval_upper_bound = 0
-      scaling_adjustment          = -1
-    }
-  }
-
-  depends_on = [aws_appautoscaling_target.radius_ecs_target_radiusentication_api]
-}
-*/
