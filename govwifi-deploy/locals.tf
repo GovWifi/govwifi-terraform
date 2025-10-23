7d7af1f91352b97c957fc43e0dcdb1c374dfc708
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2025-10-23 15:40:05 +0100

    Remove ecr repos for alpaca
    Also remove pipelines

--- govwifi-deploy/locals.tf
+++ govwifi-deploy/locals.tf
@@ -7,10 +7,6 @@ locals {
 }
 
 locals {
-  aws_alpaca_account_id = jsondecode(data.aws_secretsmanager_secret_version.alpaca_aws_account_no.secret_string)["account-id"]
-}
-
-locals {
   aws_development_account_id = jsondecode(data.aws_secretsmanager_secret_version.development_aws_account_no.secret_string)["account-id"]
 }
 
