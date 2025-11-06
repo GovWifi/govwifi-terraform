d0a439bf08855e685b4b7810480586cafd17d81a
Camille Descartes <camille.descartes@digital.cabinet-office.gov.uk>
2025-11-06 14:07:53 +0000

    Add new properties to modules in production
    This related to the changes needed to bring up
    an environment cleanly from scratch.

--- govwifi/wifi-london/main.tf
+++ govwifi/wifi-london/main.tf
@@ -78,6 +78,17 @@ data "terraform_remote_state" "dublin" {
   }
 }
 
+module "london_deployment_roles" {
+  providers = {
+    aws = aws.main
+    aws.dublin = aws.dublin
+  }
+
+  source         = "../../govwifi-deployment-roles"
+  aws_account_id = local.aws_account_id
+
+}
+
 module "govwifi_keys" {
   providers = {
     aws = aws.main
@@ -85,13 +96,8 @@ module "govwifi_keys" {
 
   source = "../../govwifi-keys"
 
-  create_production_bastion_key = 1
-
   govwifi_bastion_key_name = "govwifi-bastion-key"
   govwifi_bastion_key_pub  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDY/Q676Tp5CTpKWVksMPztERDdjWOrYFgVckF9IHGI2wC38ckWFiqawsEZBILUyNZgL/lnOtheN1UZtuGmUUkPxgtPw+YD6gMDcebhSX4wh9GM3JjXAIy9+V/WagQ84Pz10yIp+PlyzcQMu+RVRVzWyTYZUdgMsDt0tFdcgMgUc7FkC252CgtSZHpLXhnukG5KG69CoTO+kuak/k3vX5jwWjIgfMGZwIAq+F9XSIMAwylCmmdE5MetKl0Wx4EI/fm8WqSZXj+yeFRv9mQTus906AnNieOgOrgt4D24/JuRU1JTlZ35iNbOKcwlOTDSlTQrm4FA1sCllphhD/RQVYpMp6EV3xape626xwkucCC2gYnakxTZFHUIeWfC5aHGrqMOMtXRfW0xs+D+vzo3MCWepdIebWR5KVhqkbNUKHBG9e8oJbTYUkoyBZjC7LtI4fgB3+blXyFVuQoAzjf+poPzdPBfCC9eiUJrEHoOljO9yMcdkBfyW3c/o8Sd9PgNufc= bastion@govwifi"
-
-  govwifi_key_name     = var.ssh_key_name
-  govwifi_key_name_pub = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJmLa/tF941z6Dh/jiZCH6Mw/JoTXGkILim/bgDc3PSBKXFmBwkAFUVgnoOUWJDXvZWpuBJv+vUu+ZlmlszFM00BRXpb4ykRuJxWIjJiNzGlgXW69Satl2e9d37ZtLwlAdABgJyvj10QEiBtB1VS0DBRXK9J+CfwNPnwVnfppFGP86GoqE2Il86t+BB/VC//gKMTttIstyl2nqUwkK3Epq66+1ol3AelmUmBjPiyrmkwp+png9F4B86RqSNa/drfXmUGf1czE4+H+CXqOdje2bmnrwxLQ8GY3MYpz0zTVrB3T1IyXXF6dcdcF6ZId9B/10jMiTigvOeUvraFEf9fK7 govwifi@govwifi"
 }
 
 # Global ====================================================================
@@ -550,7 +556,8 @@ module "smoke_tests" {
   govwifi_phone_number       = "+447537417417"
   notify_field               = "govwifi"
   smoke_tests_repo_name      = "govwifi-smoke-tests"
-
+  govwifi_codebuild_role_name = module.london_deployment_roles.govwifi_codebuild_role_name
+  govwifi_codebuild_role_arn  = module.london_deployment_roles.govwifi_codebuild_role_arn
 
   depends_on = [
     module.frontend,
