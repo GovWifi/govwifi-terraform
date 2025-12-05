422c2fede19cd11ae3abe3fcdaed3dfa85083ed9
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2025-12-05 13:37:31 +0000

    Add in type following feedback https://github.com/GovWifi/govwifi-terraform/pull/976#discussion_r2591926876

--- govwifi-api/variables.tf
+++ govwifi-api/variables.tf
@@ -54,6 +54,7 @@ variable "user_db_hostname" {
 
 variable "db_read_replica_hostname" {
   description = "The read replica hostname to query for statistics from."
+  type        = string
 }
 
 variable "user_rr_hostname" {
