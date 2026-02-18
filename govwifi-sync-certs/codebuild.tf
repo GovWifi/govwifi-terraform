411d5963232244433b5f71c7151db0d6f816bc3f
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2026-02-18 17:57:20 +0000

    fix: [GW-2652]Fix Sync Certs

--- govwifi-sync-certs/codebuild.tf
+++ govwifi-sync-certs/codebuild.tf
@@ -10,7 +10,7 @@ resource "aws_codebuild_project" "govwifi_codebuild_sync_certs" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:6.0"
+    image                       = "aws/codebuild/standard:7.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
