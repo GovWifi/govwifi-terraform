5d2f38e30e917a86ca39eab779b94f568ef0114e
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2026-02-19 16:50:11 +0000

    make it easier to see the change :)

--- govwifi-sync-certs/codebuild.tf
+++ govwifi-sync-certs/codebuild.tf
@@ -16,11 +16,6 @@ resource "aws_codebuild_project" "govwifi_codebuild_sync_certs" {
     privileged_mode             = true
 
     environment_variable {
-      name  = "BRANCH"
-      value = "master"
-    }
-
-    environment_variable {
       name  = "DOCKER_HUB_AUTHTOKEN_ENV"
       value = data.aws_secretsmanager_secret_version.docker_hub_authtoken.secret_string
     }
@@ -56,6 +51,10 @@ resource "aws_codebuild_project" "govwifi_codebuild_sync_certs" {
       value = var.env
     }
 
+    environment_variable {
+      name  = "BRANCH"
+      value = "master"
+    }
   }
 
   source {
