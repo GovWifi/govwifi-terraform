9430ed2883b32544dc347dee59ee765e9e54a92f
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2026-04-01 17:23:00 +0100

    WIP: adding in a cluster to run metrics api

--- govwifi-api/outputs.tf
+++ govwifi-api/outputs.tf
@@ -10,3 +10,7 @@ output "authentication_api_internal_dns_name" {
 output "logging_api_internal_dns_name" {
   value = aws_lb.logging_api.*.dns_name
 }
+
+output "api_out_sg_id" {
+  value = aws_security_group.api_out.id
+}
