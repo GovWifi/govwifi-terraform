445796cb079814e3cf8e60bf465e3ab009651905
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2026-04-02 11:36:18 +0100

    Update following Leona's feedback on draft PR

--- govwifi-metrics/cluster.tf
+++ govwifi-metrics/cluster.tf
@@ -12,11 +12,6 @@ resource "aws_cloudwatch_log_group" "metrics_log_group" {
   tags = var.tags
 }
 
-locals {
-  db_creds     = jsondecode(data.aws_secretsmanager_secret_version.metrics_db_credentials_data.secret_string)
-  database_dsn = "postgres://${local.db_creds["username"]}:${local.db_creds["password"]}@${aws_rds_cluster.metrics_db_cluster.endpoint}:5432/${var.database_name}"
-}
-
 resource "aws_ecs_task_definition" "metrics_api" {
   family                   = "metrics-api-task-${var.env}"
   requires_compatibilities = ["FARGATE"]
