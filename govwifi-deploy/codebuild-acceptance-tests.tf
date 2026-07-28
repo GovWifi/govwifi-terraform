86d8816584d475d08a9f9f95db6cdcd38c036944
leonaAtkins <leona.atkins@digital.cabinet-office.gov.uk>
2026-07-28 12:59:59 +0100

    give acceptance test codebuild more ommmph!

--- govwifi-deploy/codebuild-acceptance-tests.tf
+++ govwifi-deploy/codebuild-acceptance-tests.tf
@@ -1,7 +1,7 @@
 resource "aws_codebuild_project" "govwifi_codebuild_acceptance_tests" {
   name           = "acceptance-tests"
   description    = "This project runs the frontend acceptance tests"
-  build_timeout  = "20"
+  build_timeout  = "30"
   service_role   = aws_iam_role.govwifi_codebuild.arn
   encryption_key = aws_kms_key.codepipeline_key.arn
 
@@ -10,8 +10,8 @@ resource "aws_codebuild_project" "govwifi_codebuild_acceptance_tests" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:7.0"
+    compute_type                = "BUILD_GENERAL1_MEDIUM"
+    image                       = "aws/codebuild/standard:8.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
